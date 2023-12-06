ac43e07af529aa3a0a262e74eeecaf7b13e971b3
hwicoding <dlgnl117@gmail.com>
2023-12-06 15:50:16 +0900

    이휘 : 수정 - 패키지 이름, Method 파라미터 설정

--- src/com/lec/ex2/Quiz2.java
+++ src/com/lec/ex2/Quiz2.java
@@ -1,24 +1,20 @@
-package com.Jec.ex2;
+package com.lec.ex2;
 
 public class Quiz2 {
 	
 	
 //	Field
 	
-	int num = 0;
 	
 
 	
 //	Constructor
-	public void Input(int num) {
-		this.num = num;
-		
-	}	
+
 	
 	
 	
 //	Method
-	public void gugu() {
+	public void gugu(int num) {
 		for(int time=1; time<=9; time++) {
 			System.out.println();
 			for(int dan=num; dan<=num+3; dan++) {

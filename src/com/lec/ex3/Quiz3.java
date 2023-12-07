c09a594623373a233be7a31d2a32cf4b80027620
lslh1994 <lh1994@naver.com>
2023-12-07 10:18:27 +0900

    12월 7일 이 서
    코멘트 추가

--- src/com/lec/ex3/Quiz3.java
+++ src/com/lec/ex3/Quiz3.java
@@ -2,6 +2,12 @@ package com.lec.ex3;
 
 public class Quiz3 {
 
+	/*
+     Date : 2023.12.07
+	 Author : Seo-LEE
+	 Description : 사용자에게 입력받고 퀴즈에 대한 결과 출력
+	 Version : v.1 - 코멘트 입력
+	 */
 
 	//Field
 	int fac = 1;
@@ -10,7 +16,6 @@ public class Quiz3 {
 	
 
 	//Method
-	
 	public void factorial(int num) {
 	
 		for(int i=1; i<=num; i++) {
